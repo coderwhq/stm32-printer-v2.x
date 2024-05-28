20553db0d1fc7507407b382780e300834e71dea0
Captain <2622189698@qq.com>
2024-05-28 22:02:39 +0800

    adjust include

--- User/Inc/LED.h
+++ User/Inc/LED.h
@@ -1,6 +1,9 @@
 #ifndef STM32_PRINTER_V2_X_LED_H
 #define STM32_PRINTER_V2_X_LED_H
 
+#include "main.h"
+#include "stm32f1xx_hal.h"
+
 class LED {
 public:
     LED() = default;
--- User/Src/LED.cpp
+++ User/Src/LED.cpp
@@ -1,6 +1,4 @@
 #include "LED.h"
-#include "main.h"
-#include "stm32f1xx_hal.h"
 
 void LED::ledOn() {
     HAL_GPIO_WritePin(LED_PIN_GPIO_Port, LED_PIN_Pin, ON);
